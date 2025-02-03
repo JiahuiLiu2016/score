7598d0eaea195c6c4e00c95d1f3979270ec08baa
jiahuiliu <jiahuiliu2016@gmail.com>
2025-02-03 16:34:59 +0800

    feat: 1

---
+++ src/main/java/com/ss/ScoreApplication.java
@@ -0,0 +1,38 @@
+package com.ss;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+@EnableScheduling
+@MapperScan({"com.ss.**.mapper"})
+@EnableConfigurationProperties
+@EnableAsync
+@Component
+public class ScoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScoreApplication.class, args);
+    }
+
+    /**
+     * 分页插件
+     *
+     * @return 分页插件bean
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
